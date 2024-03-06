ef67401a8808e5bb7725aeccc350a2a0b511f538
Nathan Li <moncskoli@gmail.com>
2024-03-06 10:21:05 -0500

    trying out maplibre in a new file

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,50 +1,20 @@
 <script>
-    let jsonData;
-
-    async function fetchData() {
-        try {
-            const response = await fetch(
-                "https://passio3.com/harvard/passioTransit/gtfs/realtime/vehiclePositions.json",
-            );
-            jsonData = await response.json();
-        } catch (error) {
-            console.error("Error fetching JSON data:", error);
-        }
-    }
-
-    fetchData();
+    import { MapLibre } from "svelte-maplibre";
 </script>
 
 <main>
-    <head>
-        <link
-            rel="stylesheet"
-            href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-        />
-    </head>
-    <div id="map"></div>
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-    <script>
-        // Map initialization
-        var map = L.map('map').setView([51.505, -0.09], 13);
-        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 19,
-            attribution:
-                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        }).addTo(map);
-    </script>
-    {#if jsonData}
-        <div>
-            <!-- Render your JSON data here -->
-            <pre>{JSON.stringify(jsonData, null, 2)}</pre>
-        </div>
-    {:else}
-        <p>Loading...</p>
-    {/if}
+    <MapLibre
+        center={[-71.1189, 42.3735]}
+        zoom={13}
+        class="map"
+        standardControls
+        style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+    />
 </main>
 
 <style>
-    #map {
+    :global(.map) {
         height: 500px;
+        width: 1000px;
     }
 </style>
---
+++ my-app/src/routes/oldpage.svelte
@@ -0,0 +1,57 @@
+<script>
+    import Geolocation from "svelte-geolocation";
+
+    let getPosition = true;
+    let coords = [];
+    let jsonData;
+
+    async function fetchData() {
+        try {
+            const response = await fetch(
+                "https://passio3.com/harvard/passioTransit/gtfs/realtime/vehiclePositions.json",
+            );
+            jsonData = await response.json();
+        } catch (error) {
+            console.error("Error fetching JSON data:", error);
+        }
+    }
+
+    fetchData();
+</script>
+
+<Geolocation getPosition="{getPosition}" bind:coords />
+
+<main>
+    <pre>{JSON.stringify(coords)}</pre>
+    <head>
+        <link
+            rel="stylesheet"
+            href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        />
+    </head>
+    <div id="map"></div>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script>
+        // Map initialization
+        var map = L.map('map').setView([42.3735, 71.1189], 13);
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution:
+                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(map);
+    </script>
+    {#if jsonData}
+        <div>
+            <!-- Render your JSON data here -->
+            <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+        </div>
+    {:else}
+        <p>Loading...</p>
+    {/if}
+</main>
+
+<style>
+    #map {
+        height: 500px;
+    }
+</style>
