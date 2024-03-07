92cff9df82e293d52d4ad5c764b65051431dcd71
Nathan Li <moncskoli@gmail.com>
2024-03-06 21:49:26 -0500

    now displaying all active shuttles

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script>
     import { onMount } from "svelte";
 
-    var jsonData;
-    var bus_loc;
-    var marker;
+    let jsonData;
+    let markers = [];
 
     onMount(() => {
         async function fetchData() {
@@ -12,9 +11,7 @@
                     "https://passio3.com/harvard/passioTransit/gtfs/realtime/vehiclePositions.json",
                 );
                 jsonData = await response.json();
-                bus_loc = jsonData.entity[0].vehicle.position;
-                bus_loc = [bus_loc.longitude, bus_loc.latitude];
-                setMarker();
+                updateMarkers();
             } catch (error) {
                 console.error("Error fetching JSON data:", error);
             }
@@ -26,14 +23,17 @@
         return () => clearInterval(interval);
     });
 
-    function setMarker() {
-        if (bus_loc) {
-            if (marker) {
-                marker.setLngLat(bus_loc)
-            }
-            else {
-                marker = new maplibregl.Marker().setLngLat(bus_loc).addTo(map);
-            }
+    function updateMarkers() {
+        if (jsonData && jsonData.entity) {
+            markers.forEach(marker => marker.remove());
+            markers = [];
+
+            jsonData.entity.forEach(entity => {
+                const bus_loc = entity.vehicle.position;
+                const lngLat = [bus_loc.longitude, bus_loc.latitude];
+                const marker = new maplibregl.Marker().setLngLat(lngLat).addTo(map);
+                markers.push(marker);
+            });
         }
     }
 </script>
@@ -49,17 +49,16 @@
         />
     </head>
     <body>
-        <div id="map" style="width: 1000px; height: 500px;"></div>
+        <div id="map" style="width: 800px; height: 700px;"></div>
         <script>
             var map = new maplibregl.Map({
                 container: "map",
                 style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json", // stylesheet location
                 center: [-71.1189, 42.3735], // starting position [lng, lat]
-                zoom: 13, // starting zoom
+                zoom: 14, // starting zoom
             });
         </script>
         {#if jsonData}
-            <p>{bus_loc}</p>
             <div>
                 <!-- Render your JSON data here -->
                 <pre>{JSON.stringify(jsonData, null, 2)}</pre>
