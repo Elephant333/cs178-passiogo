feebd8629f1acd3e2d5b129d7b4ba14da5578b03
Nathan Li <moncskoli@gmail.com>
2024-03-11 18:41:34 -0400

    faster refresh, 3s to 1s

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -121,7 +121,7 @@
         fetchRouteToName();
         fetchStopToName();
 
-        const interval = setInterval(fetchData, 3000);
+        const interval = setInterval(fetchData, 1000);
 
         return () => clearInterval(interval);
     });
