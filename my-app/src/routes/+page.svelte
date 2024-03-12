020e8d10ba304fcf714066c074c0ed80f30e4875
Nathan Li <moncskoli@gmail.com>
2024-03-12 11:57:03 -0400

    added accordian from svelte material library

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
     import { onMount } from "svelte";
     import maplibregl from "maplibre-gl";
+    import Accordion, { Panel, Header, Content } from "@smui-extra/accordion";
+    import IconButton, { Icon } from "@smui/icon-button";
+
+    let panel1Open = false;
+    let panel2Open = false;
+    let panel3Open = false;
+    let panel4Open = false;
 
     let jsonGPSData;
     let jsonETAData;
@@ -390,6 +397,52 @@
     <body>
         <div class="container">
             <div class="sidebar">
+                <div class="accordion-container">
+                    <Accordion>
+                        <Panel bind:open={panel1Open}>
+                            <Header>
+                                Panel 1
+                                <IconButton slot="icon" toggle pressed={panel1Open}>
+                                    <Icon class="material-icons" on>expand_less</Icon>
+                                    <Icon class="material-icons">expand_more</Icon>
+                                </IconButton>
+                            </Header>
+                            <Content>The content for panel 1.</Content>
+                        </Panel>
+                        <Panel bind:open={panel2Open}>
+                            <Header>
+                                Panel 2
+                                <IconButton slot="icon" toggle pressed={panel2Open}>
+                                    <Icon class="material-icons" on>expand_less</Icon>
+                                    <Icon class="material-icons">expand_more</Icon>
+                                </IconButton>
+                            </Header>
+                            <Content>The content for panel 2.</Content>
+                        </Panel>
+                        <Panel bind:open={panel3Open}>
+                            <Header>
+                                Panel 3
+                                <span slot="description">Description of panel 3.</span>
+                                <IconButton slot="icon" toggle pressed={panel3Open}>
+                                    <Icon class="material-icons" on>expand_less</Icon>
+                                    <Icon class="material-icons">expand_more</Icon>
+                                </IconButton>
+                            </Header>
+                            <Content>The content for panel 3.</Content>
+                        </Panel>
+                        <Panel bind:open={panel4Open}>
+                            <Header>
+                                Panel 4
+                                <span slot="description">Description of panel 4.</span>
+                                <IconButton slot="icon" toggle pressed={panel4Open}>
+                                    <Icon class="material-icons" on>expand_less</Icon>
+                                    <Icon class="material-icons">expand_more</Icon>
+                                </IconButton>
+                            </Header>
+                            <Content>The content for panel 4.</Content>
+                        </Panel>
+                    </Accordion>
+                </div>
                 <h2>ETAs Nearest to You</h2>
                 {#if trip_to_route && route_to_name && stop_dict}
                     {#each closestETATimes as routeETA}
@@ -488,6 +541,8 @@
 
     .sidebar {
         padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 20px;
         width: 400px;
         height: 700px;
         background-color: lightgray;
